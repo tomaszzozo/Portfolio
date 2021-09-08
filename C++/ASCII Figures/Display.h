5c986cd891acb0628a4065fea312d8446038777b
Tomasz.Wojtkiewicz <59620464+tomaszzozo@users.noreply.github.com>
2021-09-08 19:15:58 +0200

    Delete C++/ASCII Figures directory

--- C++/ASCII Figures/Display.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class Display
-{
-public:
-    virtual void drawTriangle(int a1, int a2, int b1, int b2) = 0;
-    virtual void drawCircle(int r) = 0;
-    virtual void drawParallelogram(int a1, int a2,int b1,int b2) = 0;
-};
--- C++/ASCII Figures/MyExcpetions.cpp
+++
@@ -1,6 +0,0 @@
-#include "MyExcpetions.h"
-
-const char *MyExcpetions::what() const noexcept
-{
-    return "Incorrect arguments!";
-}
